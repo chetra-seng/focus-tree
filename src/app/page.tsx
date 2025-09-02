5284f57ed438f183e86c4739f3253e6e699a0a53
Chetra Seng <dev@chetraseng.com>
2025-09-02 17:13:05 +0700

    disable turbopack for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import FocusTree from "@/components/focus-tree";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <FocusTree />;
+  return (
+    <Suspense>
+      <FocusTree />
+    </Suspense>
+  );
 }
